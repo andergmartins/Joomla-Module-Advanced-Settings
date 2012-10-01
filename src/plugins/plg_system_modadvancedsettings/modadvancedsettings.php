052c7303f62c7617d14c7806474da4e86b003834
Anderson Grudtner Martins <andergmartins@gmail.com>
2012-10-01 17:50:59 -0300

    Add a comment about Joomla 2.5.7 test.

--- src/plugins/plg_system_modadvancedsettings/modadvancedsettings.php
+++ src/plugins/plg_system_modadvancedsettings/modadvancedsettings.php
@@ -10,6 +10,7 @@ defined('_JEXEC') or die;
 jimport( 'joomla.plugin.plugin' );
 
 // Load modified JDocumentRendererModule before the Joomla Framework doest it, to ignore that
+// Works with Joomla 2.5.7 too.
 require_once JPATH_SITE.DS.'plugins'.DS.'system'.DS.'modadvancedsettings'.DS.'class'.DS.'jmodulehelper-2.5.6.php';
 
 
